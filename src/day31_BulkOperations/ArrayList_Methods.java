a7bf293727d206e9d81937bd31f0e01d2eae4286
ogulle <oigulle@gmail.com>
2020-05-05 14:14:58 -0500

    Day31

--- src/day31_BulkOperations/ArrayList_Methods.java
+++ src/day31_BulkOperations/ArrayList_Methods.java
@@ -64,6 +64,12 @@ public class ArrayList_Methods {
 
         System.out.println(list3);
 
+        System.out.println(list3.size());
+
+        list3.clear();  //removes everything
+        boolean r4 = list3.isEmpty();
+        System.out.println(r4);
+
 
     }
 }
